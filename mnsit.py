763f1eba5719566b78350e171f003e293f1819d4
sushmaunnibhavi <sushmaunnibhavi@gmail.com>
2019-06-12 13:24:50 +0530

    made some changes

---
+++ mnsit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 13:04:35 2019
+
+@author: sushma
+"""
+
+#Handwritten digit recognition using convolutional neural networks(CNN)
+#Here we are traininng a multi level neural network to recognize the digits
+#Here we are using a database called MNIST which contains 60000 handwritten digits with their labels
+#After training the neural network,it should now classify the new image correctly
+
+
+#Step 1:download the database of images from MNIST
+#Step 2:set up a neural network with required number of layers and nodes and train the network
+#Step 3:feed the training data to the neural network
+#Step 4:we will check what the output will be like for one image
+#Step 5:feed the test data set of 10000 images to trained neural network and check its accuracy
+
+
+#Step 1:
+
+
